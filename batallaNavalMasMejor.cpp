9465b64f6b3e658ca9dcdd10aea9d247c3fb2da9
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 <lautaroolmedo77@gmail.com>
2023-06-11 22:26:08 -0300

    correcion puntaje

--- batallaNavalMasMejor.cpp
+++ batallaNavalMasMejor.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
-
+#include <cctype>
+#include <stdio.h>
 using namespace std;
 
 
 struct jugador {
     string nombre;
     int puntos;
+    int victorias;
     string tablero[11][11]={{"   ","A","B","C","D","E","F","G","H","I","J"},
                         {"1  ","0","0","0","0","0","0","0","0","0","0"},
                         {"2  ","0","0","0","0","0","0","0","0","0","0"},
@@ -17,7 +19,7 @@ struct jugador {
                         {"7  ","0","0","0","0","0","0","0","0","0","0"},
                         {"8  ","0","0","0","0","0","0","0","0","0","0"},
                         {"9  ","0","0","0","0","0","0","0","0","0","0"},
-                        {"10 ","0","0","0","0","0","0","0","0","0","0"},};;
+                        {"10 ","0","0","0","0","0","0","0","0","0","0"},};
     string marcador[11][11]={{"   ","A","B","C","D","E","F","G","H","I","J"},
                         {"1  ","0","0","0","0","0","0","0","0","0","0"},
                         {"2  ","0","0","0","0","0","0","0","0","0","0"},
@@ -28,10 +30,11 @@ struct jugador {
                         {"7  ","0","0","0","0","0","0","0","0","0","0"},
                         {"8  ","0","0","0","0","0","0","0","0","0","0"},
                         {"9  ","0","0","0","0","0","0","0","0","0","0"},
-                        {"10 ","0","0","0","0","0","0","0","0","0","0"},};;
+                        {"10 ","0","0","0","0","0","0","0","0","0","0"},};
+   
 };
 
-//void recorrerTableros();
+void recorrerTableros();
 void pintarTablero(string tableroJugadorPintar[11][11]);
 void cargarBarco(string tableroACargar[11][11]);
 int preguntarPosBarco(int& linea , int& columna);
@@ -40,15 +43,27 @@ bool validacionesDir(int linea, int columna,string tableroDireccion[11][11],int 
 void colocarBarcoDir(int linea,int columna, int largoBarco,string dir, string tableroBarco[11][11]);
 string direccionBarco(string& dirBarco);
 string cambiarNumALetra(int largoBarco);
-int atacar(string tablero[11][11], string marcador[11][11], jugador jugadorAtacante);
-//void dondeAtacar(string marcadorAtaque[11][11]);
+
+// ---------- ATACAR ----------
+
+int atacar(string tablero[11][11], string marcador[11][11], jugador jugadorAtacante, jugador jugadorAtacado);
+void dondeAtacar(string marcadorAtaque[11][11]);
 float verificacionAtaque(string marcadorAtaque[11][11], int fila, int columna);
 float verificarFila(int fila);
 float verificarColumna(int columna);
 float verificacionFilaYColumna(int fila, int columna);
+void contMarcador(int columna,int fila, string jugadorMarcador[11][11], jugador jugadorCont );
+void barcoHundido(int cont2, int cont3, int cont4, int cont5, jugador jugadorHundido);
+void jugadorVictorias(jugador victoriasTotales, jugador miJugador);
 float verificarQuienGana(int puntosjugador1, int puntosjugador2);
+void reiniciarJugador(jugador miJugador);
+
+// ---------- ATACAR ----------
+// ---------- JUGAR ----------
+
 void jugar();
 
+// ---------- JUGAR ----------
 
 int main(){                      
     jugar();
@@ -207,11 +222,15 @@ return tamano;
 }
 
 string direccionBarco(string& dirBarco){
-    
+    string toLowerWord;
 	cout<<"Para que direccion quiere que vaya el barco(derecha, izquierda, arriba o abajo): "<<"\n";
     cin>> dirBarco;
- 	
-    return dirBarco;
+    
+  for (int i=0; i<dirBarco.length(); i++){
+    	dirBarco[i] = tolower(dirBarco[i]);
+	}
+    
+	return dirBarco;
  	
 }
 
@@ -247,7 +266,7 @@ float transformarColumnas(char columna){
 
 // ---------- ATACAR ----------
 
-int atacar(string tableroAtacado[11][11], string marcadorAtaque[11][11], jugador jugadorAtacante){ // ---> RECIBE LOS ARREGLOS CON LOS BARCOS CARGADOS 
+int atacar(string tableroAtacado[11][11], string marcadorAtaque[11][11], jugador jugadorAtacante, jugador jugadorAtacado){ // ---> RECIBE LOS ARREGLOS CON LOS BARCOS CARGADOS 
     bool fallo = true;
     int fila, columna;
     bool verificacion;    //, verificacionFila, verificacionColumna;
@@ -261,9 +280,7 @@ int atacar(string tableroAtacado[11][11], string marcadorAtaque[11][11], jugador
 	do{
 	cout << "Ingrese la fila donde queres atacar" <<"\n";    //Si en la fila pones una letra salta un bucle.
 	cin >> fila;
-
-	cout << "\n";
-
+	
 	cout << "Ingrese la columna donde queres atacar" <<"\n";
 	cin>>letraColumna;
     
@@ -271,13 +288,19 @@ int atacar(string tableroAtacado[11][11], string marcadorAtaque[11][11], jugador
 	
 	}while(!verificacionFilaYColumna(fila, columna) || !verificacionAtaque(marcadorAtaque, fila, columna));
 	
+	contMarcador(columna,fila,tableroAtacado,jugadorAtacado);
 	marcadorAtaque[fila][columna] = "*";
-
+	// Aca termina la funcion que era dondeAtacar
+	
+	// sacamos los for y solo revisamos donde el usuario eligio atacar
+	//for(int i = 0; i < 11; i++){
+	//	for(int t = 0; t < 11; t++){
 	if(tableroAtacado[fila][columna] != "0"){
 		cout << "LE DISTE A UN BARCO \n";
 		tableroAtacado[fila][columna] = "X";
 		marcadorAtaque[fila][columna] = "R";
 		pintarTablero(marcadorAtaque);
+		
 		return 1;
 
 	}else{
@@ -285,6 +308,7 @@ int atacar(string tableroAtacado[11][11], string marcadorAtaque[11][11], jugador
 		fallo = false;
 				
 	}
+
 	if(!fallo){
 		cout << "NO LE PEGASTE AL BARCO \n";
 		return 0;
@@ -339,39 +363,133 @@ float verificarQuienGana(int puntosjugador1, int puntosjugador2){
 	}
 }
 
+void contMarcador(int columna,int fila, string jugadorMarcador[11][11], jugador jugadorCont ){
+	
+int cont2, cont3, cont4, cont5 ;
+    
+    cont2=0;
+    cont3=0;
+    cont4=0;
+    cont5=0;
+	
+if(jugadorMarcador[fila][columna] == "2"){
+	cont2 ++;
+	barcoHundido(cont2,cont3,cont4,cont5,jugadorCont);
+}else if(jugadorMarcador[fila][columna] == "3"){
+    cont3 ++;
+    barcoHundido(cont2,cont3,cont4,cont5,jugadorCont);
+}else if(jugadorMarcador[fila][columna] == "4"){
+	cont4 ++;
+	barcoHundido(cont2,cont3,cont4,cont5,jugadorCont);
+}else if(jugadorMarcador[fila][columna] == "5"){
+	cont5 ++;
+	barcoHundido(cont2,cont3,cont4,cont5,jugadorCont);
+	}
+		
+ 	 
+}
+
+void barcoHundido(int cont2, int cont3, int cont4, int cont5, jugador jugadorHundido ){
+	if(cont2=2){
+		cout<<"HUNDISTE EL BARCO DE "<< cont2 <<" DE LARGO DEL JUGADOR " << jugadorHundido.nombre <<"\n";
+	}else if(cont3=3){
+		cout<<"HUNDISTE EL BARCO DE "<< cont3 <<" DE LARGO DEL JUGADOR " << jugadorHundido.nombre <<"\n";
+	}else if(cont4=4){
+		cout<<"HUNDISTE EL BARCO DE "<< cont4 <<" DE LARGO DEL JUGADOR " << jugadorHundido.nombre <<"\n";
+	}else if(cont5=5){
+		cout<<"HUNDISTE EL BARCO DE "<< cont5 <<" DE LARGO DEL JUGADOR " << jugadorHundido.nombre <<"\n";
+	}
+}
+
+
+	
+
+
+// ---------- ATACAR ----------
+
+// ---------- REINICIAR ----------- 
+void reiniciarJugador(jugador miJugador){
+	
+	miJugador.puntos=0;
+	 string matriz [11][11]={{"   ","A","B","C","D","E","F","G","H","I","J"},
+                        {"1  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"2  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"3  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"4  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"5  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"6  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"7  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"8  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"9  ","0","0","0","0","0","0","0","0","0","0"},
+                        {"10 ","0","0","0","0","0","0","0","0","0","0"},};
+	
+                        
+    for(int i = 0; i < 11; i++){
+    	for(int t = 0; t < 11; t++){
+    		miJugador.tablero[i][t] = matriz[i][t];
+		}
+	}
+	for(int i = 0; i < 11; i++){
+    	for(int t = 0; t < 11; t++){
+    		miJugador.marcador[i][t] = matriz[i][t];
+		}
+	}
+
+}
+
+// ---------- JUGAR ----------
+
 void jugar(){
 	int contadorJugador1, contadorJugador2, opcionMenuJugar;
 	jugador jugador1, jugador2;
 
-	cout << "Bienvenida/os a la Batalla Naval!!" << "\n";
+	
 
-	//do{
+printf("    ____        __        ____         _   __                  __\n");
+printf("   / __ )____ _/ /_____ _/ / /___ _   / | / /___ __   ______ _/ /\n");
+printf("  / __  / __ `/ __/ __ `/ / / __ `/  /  |/ / __ `/ | / / __ `/ / \n");
+printf(" / /_/ / /_/ / /_/ /_/ / / / /_/ /  / /|  / /_/ /| |/ / /_/ / /  \n");
+printf("/_____/|__,_/___/|__,_/_/_/|__,_/  /_/ |_/|__,_/ |___/|__,_/_/   \n");
+                                                                 
+
+                                                          
+
+    
+	cout << "Ingresa el nombre del primer jugador" << "\n";
+	cin >> jugador1.nombre;
+	cout << "Ingresa el nombre del segundo jugador" << "\n";
+	cin >> jugador2.nombre;
+	
+	jugador1.victorias = 0;
+    jugador2.victorias = 0;
+    
+	do{
 	
 	do{
 		cout << "ELegir una opcion" << "\n";
 		cout << "1. Jugar" << "\n";
-		cout << "2. Salir" << "\n";
+		cout << "2. Puntaje" << "\n";
+		cout << "3. Salir" << "\n";
 		cin >> opcionMenuJugar;
-		if (opcionMenuJugar != 1 && opcionMenuJugar !=2){
+		if (opcionMenuJugar != 1 && opcionMenuJugar !=2 && opcionMenuJugar !=3){
 			cout << "Opcion incorrecta. Por favor, elija una opcion valida. \n";
 		}
-	} while (opcionMenuJugar != 1 && opcionMenuJugar !=2);
+	} while (opcionMenuJugar != 1 && opcionMenuJugar !=2 && opcionMenuJugar !=3);
 
 
-	if(opcionMenuJugar == 2){
+	if(opcionMenuJugar == 3){
 		cout << "Elegiste SALIR ";
 		return;
-	}else{
+	}else if (opcionMenuJugar == 1){
 
 		cout << "Elegiste JUGAR" << "\n";
 
-		cout << "Ingresa el nombre del primero jugador" << "\n";
-		cin >> jugador1.nombre;
-		cout << "Ingresa el nombre del segundo jugador" << "\n";
-		cin >> jugador2.nombre;
+		
 
 		jugador1.puntos = 0;
         jugador2.puntos = 0;
+        jugador1.victorias = 0;
+        jugador2.victorias = 0;
 
 		cout <<"\n";
 		cout << "Tablero de " << jugador1.nombre<<"\n";
@@ -386,6 +504,16 @@ void jugar(){
 
 		cout <<"\n";
 		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
+		cout <<"\n";
 
 		cout << "Tablero de " << jugador2.nombre<<"\n";
 		cout <<"\n";
@@ -393,26 +521,44 @@ void jugar(){
 
 		cout <<"\n";
 
-		cout << "Turno de " << jugador1.nombre <<"\n";
+		cout << "Turno de " << jugador2.nombre <<"\n";
 		cout <<"\n";
 	    cargarBarco(jugador2.tablero);
 	do{
 		        
-	    jugador1.puntos = jugador1.puntos + atacar(jugador2.tablero, jugador1.marcador, jugador1);
+	    jugador1.puntos = jugador1.puntos + atacar(jugador2.tablero, jugador1.marcador, jugador1, jugador2);
 		cout <<"\n";
 		cout <<"\n";
-		jugador2.puntos = jugador2.puntos + atacar(jugador1.tablero, jugador2.marcador, jugador2);
+		jugador2.puntos = jugador2.puntos + atacar(jugador1.tablero, jugador2.marcador, jugador2, jugador1);
 		cout << "El jugador " << jugador1.nombre << ". Lleva " << jugador1.puntos << " puntos de 14 \n";
 		cout << "El jugador " << jugador2.nombre << ". Lleva " << jugador2.puntos << " puntos de 14 \n" ;
 	    }while(verificarQuienGana(jugador1.puntos, jugador2.puntos));   
+	}else if(opcionMenuJugar == 2){
+		cout<<"EL JUGADOR "<< jugador1.nombre <<" HA GANADO "<< jugador1.victorias <<" VECES. \n";
+		cout<<"EL JUGADOR "<< jugador2.nombre <<" HA GANADO "<< jugador2.victorias <<" VECES. \n";
+		
 	}
 
 	if(jugador1.puntos == 14){
 			cout << "EL jugador " << jugador1.nombre << " ES EL GANADOR!!" << "\n";
-		}else{
+			cout << "FIN DE LA PARTIDA";
+		}else if (jugador2.puntos == 14){
 			cout << "EL jugador " << jugador2.nombre << " ES EL GANADOR!!" << "\n";
+			cout << "FIN DE LA PARTIDA";
 	}
-	cout << "ADIOS";
+
+	
+	
+	if(jugador1.puntos==14){
+	jugador1.victorias ++;
+	}else if(jugador2.puntos==14){
+		jugador2.victorias ++;
+	}
+	
+	reiniciarJugador(jugador1);
+	reiniciarJugador(jugador2);
+	
+	}while(opcionMenuJugar != 3);
 }
 
-
+// ---------- JUGAR ----------
